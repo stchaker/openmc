19cee3584a30fe67b16cceb4fbf64b3d95d75e6a
jstchakerian <jstchakerian@lanl.gov>
2025-07-08 17:00:38 -0600

    Fix introduced bugs.

--- src/particle.cpp
+++ src/particle.cpp
@@ -75,7 +75,7 @@ double Particle::speed() const
   }
 }
 
-bool Particle::p.create_secondary(p.wgt(), p.u(), p.E(), ParticleType::neutron);(
+bool Particle::create_secondary(
   double wgt, Direction u, double E, ParticleType type)
 {
   // If energy is below cutoff for this particle, don't create secondary
