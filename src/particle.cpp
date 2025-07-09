b81d66dff630584943d3ac6e6fdf59cf6c7263e0
jstchakerian <jstchakerian@lanl.gov>
2025-07-09 12:05:25 -0600

    Modify sanity check epsilon to be a bit smaller.

--- src/particle.cpp
+++ src/particle.cpp
@@ -599,7 +599,7 @@ void Particle::pht_secondary_particles()
 
 void Particle::sanity_check(){
   const double godiva_rad = 8.7407;
-  const double epsilon = 0.00001; 
+  const double epsilon = 0.0000001; 
   if(r().norm() > (godiva_rad + epsilon) && (material() != MATERIAL_VOID)){
     std::cout << "Particle position norm is: " << r().norm() << " Which is outside of: " << godiva_rad << '\n';
     std::cout << "Particle material is: " << material() << " Which is not: " << MATERIAL_VOID << '\n';
