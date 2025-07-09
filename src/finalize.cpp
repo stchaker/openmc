39cc35bf80906f903ace6b14aa3cd0f4800be623
jstchakerian <jstchakerian@lanl.gov>
2025-07-09 16:49:56 -0600

    Remove clearing of memory from counter tallies

--- src/finalize.cpp
+++ src/finalize.cpp
@@ -157,9 +157,6 @@ int openmc_finalize()
   simulation::entropy_mesh = nullptr;
   simulation::ufs_mesh = nullptr;
 
-  simulation::fission_counter_tally.clear();
-  simulation::alpha_counter_tally.clear(); 
-
   data::energy_max = {INFTY, INFTY};
   data::energy_min = {0.0, 0.0};
   data::temperature_min = 0.0;
