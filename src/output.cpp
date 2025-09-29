0d6fa260a6ac8a35ce8b108382ea8a4f199e06f8
jstchakerian <jstchakerian@lanl.gov>
2025-09-29 11:54:31 -0600

    Only output to screen alpha once active batches begin.

--- src/output.cpp
+++ src/output.cpp
@@ -406,11 +406,6 @@ void print_generation()
     fmt::print("   {:8.5f} +/-{:8.5f}", simulation::keff, simulation::keff_std);
   }
 
-
-  if(settings::run_mode == RunMode::ALPHA && n <= 1) {
-    fmt::print("                              {:8.9f}", simulation::alpha_eigenvalue/1E6);
-  }
-
   if (n > 1 && settings::run_mode == RunMode::ALPHA) {
     fmt::print("       {:8.9f}                {:8.9f},", simulation::alpha_eigenvalue/1E6, simulation::alpha_eigenvalue_average/1E6 );
   }
